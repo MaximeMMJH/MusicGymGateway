1d9ebd49497c7b625bec8e660163ee32d3784694
Heuver,Maxime M.M.J <maxime.heuver@gmail.com>
2021-12-17 13:38:50 +0100

    ping

---
+++ Controllers/PingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicGymGateway.Controllers
+{
+    public class PingController : Controller
+    {
+        [HttpGet]
+        [Route("/ping")]
+        public IActionResult Ping()
+        {
+            return Ok("Hello world!");
+        }
+    }
+}
